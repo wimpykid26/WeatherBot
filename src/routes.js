4fa7cc8a409b5ab3d770608d30c41cb5bbcdfd6c
wimpykid26 <dwaipayan2609@gmail.com>
2019-05-19 18:42:59 +0530

    Initial slack response added

---
+++ src/routes.js
@@ -0,0 +1,28 @@
+import express from 'express';
+
+import { log } from './utils';
+
+const router = new express.Router();
+
+router.post('/slack/command/weather', async (req, res) => {
+    try {
+        const slackReqObj = req.body;
+        const response = {
+            response_type: 'in_channel',
+            channel: slackReqObj.channel_id,
+            text: 'Hello :slightly_smiling_face:',
+            attachments: [{
+                text: 'Hello, what do you want to know about the weather today',
+                fallback: 'Hello, what do you want to know about the weather today',
+                color: '#2c963f',
+                attachment_type: 'default',
+            }],
+        };
+        return res.json(response);
+    } catch (err) {
+        log.error(err);
+        return res.status(500).send('Something blew up. We\'re looking into it.');
+    }
+});
+
+export default router;
