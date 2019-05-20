08567c94d46deddfd35fbb7c8d946d23e5ef74fa
wimpykid26 <dwaipayan2609@gmail.com>
2019-05-20 16:03:34 +0530

    Added weather controller for single city

--- src/modules/weather/index.js
+++ src/modules/weather/index.js
@@ -1,4 +1,5 @@
 let rp = require('request-promise');
+import { parseOpenWeatherResponse, constructSlackResponse } from '../../utils';
 export const getIntentEntity = async (slackReqBody) => {
     try {
         var options = {
@@ -24,3 +25,55 @@ export const getIntentEntity = async (slackReqBody) => {
         throw err;
     }
 };
+
+export const parseLUISResponse = (LUISResponse) => {
+    intent = LUISResponse.topScoringIntent.intent;
+    entities = [];
+    LUISResponse.entities.map(function parseEntity(entity) {
+        if (entity.type.indexOf('geographyV2') > -1) {
+            entities.push({ type: 'City', value: entity.entity });
+        } else if (entity.type.indexOf('datetimeV2') > -1) {
+            entities.push({ type: 'Date', value: entity.entity });
+        };
+    });
+    return { intent, entities }
+};
+
+export const weatherController = async (LUISResponse, channelId) => {
+    let { intent, entities } = parseLUISResponse(LUISResponse);
+    if (intent == 'Weather.CheckWeatherValue' || 'Weather.QueryWeather') {
+        //Check for places, if not use current LatLng
+        let placeIndex = entities.findIndex(x => x.type == 'City');
+        if (placeIndex > -1) {
+            //Call api for city
+            let city = entities[placeIndex].value;
+            try {
+                var options = {
+                    uri: 'api.openweathermap.org/data/2.5/weather',
+                    qs:
+                    {
+                        'APPID': 'e101c80f28e7c194f52d9415e4be26f1',
+                        q: city
+                    },
+                };
+                return new Promise((resolve, reject) => {
+                    rp(options)
+                        .then(function (openWeatherResponse) {
+                            //Construct Slack Response
+                            attachment = parseOpenWeatherResponse(openWeatherResponse, city, 'celsius');
+                            SLACKResponse = constructSlackResponse(intent, attachment, channelId);
+                            resolve(SLACKResponse)
+                        })
+                        .catch(function (err) {
+                            reject(err);
+                        });
+                });
+            } catch (err) {
+                throw err;
+            }
+        }
+        else {
+            //Call api with LatLng
+        }
+    }
+}
